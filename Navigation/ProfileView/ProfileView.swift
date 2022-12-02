0c8f1a913a7f04a5540acd165eecbca39fe3593c
Sidorik Ilia <iss19.spb@gmail.com>
2022-12-02 13:07:19 +0700

    add new storyboard and controller and photo and view

---
+++ Navigation/ProfileView/ProfileView.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileView.swift
+//  Navigation
+//
+//  Created by Илья Сидорик on 30.11.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    
+}
---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Илья Сидорик on 30.11.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    
+    
+}
