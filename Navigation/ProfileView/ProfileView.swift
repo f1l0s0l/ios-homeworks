4d10955d43ecda547c0823ed7eb9c84aa581ffb6
Sidorik Ilia <iss19.spb@gmail.com>
2022-12-02 13:53:41 +0700

    Add UI items in ProfileView

--- Navigation/ProfileView/ProfileView.swift
+++ Navigation/ProfileView/ProfileView.swift
@@ -8,6 +8,30 @@
 import UIKit
 
 class ProfileView: UIView {
+ 
+    @IBOutlet weak var userPhoto: UIImageView! {
+        didSet {
+            userPhoto.image = UIImage(named: "Daughter")
+        }
+    }
     
+    @IBOutlet weak var userName: UILabel! {
+        didSet {
+            userName.text = "Имя"
+        }
+    }
     
+    @IBOutlet weak var userDateAge: UILabel! {
+        didSet {
+            userDateAge.text = "Дата рождения"
+        }
+    }
+    
+    @IBOutlet weak var userSity: UILabel! {
+        didSet {
+            userSity.text = "Город"
+        }
+    }
+    
+    @IBOutlet weak var userText: UITextView!
 }
--- Navigation/ProfileViewController.swift
+++ Navigation/ProfileViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadNib()
+    }
     
+    
+    func loadNib() {
+        if let nibView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
+            nibView.frame = CGRect(x: 0, y: 0, width: 0, height: 0 )
+            view.addSubview(nibView)
+        }
+    }
     
 }
+
